2a43414420fccf09951fe06e3bc16fb12147ec48
Programmer Muslim <rizki@imastudio.co.id>
2018-04-24 21:25:59 +0700

    add

--- app/src/main/java/superheroku/id/co/androidkotlin6listview/MainActivity.kt
+++ app/src/main/java/superheroku/id/co/androidkotlin6listview/MainActivity.kt
@@ -2,6 +2,11 @@ package superheroku.id.co.androidkotlin6listview
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +16,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = ArrayAdapter(this,
+                R.layout.listview_item, array)
+
+        val listView: ListView = findViewById(R.id.listview_1)
+        listView.setAdapter(adapter)
+
+        listView.onItemClickListener = object : AdapterView.OnItemClickListener {
+
+            override fun onItemClick(parent: AdapterView<*>, view: View,
+                                     position: Int, id: Long) {
+
+                // value of item that is clicked
+                val itemValue = listView.getItemAtPosition(position) as String
+
+                // Toast the values
+                Toast.makeText(applicationContext,
+                        "Position :$position\nItem Value : $itemValue", Toast.LENGTH_LONG)
+                        .show()
+            }
+        }
     }
 }
